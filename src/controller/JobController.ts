5d9768127f06d8a3301a3aa119a3735a407d428d
jolimba <joovictors85@gmail.com>
2026-03-17 22:19:12 -0300

    simple fix

--- src/controller/JobController.ts
+++ src/controller/JobController.ts
@@ -5,25 +5,25 @@ import { ChangeType } from "../@types/ChangeStatus"
 const service = new VideoService();
 
 export class JobController {
-    async list(req: Request<{ id: string }>, res: Response) {
+    async list(req: Request<{ id: string }>, res: Response): Promise<Response> {
         const id = req.params.id;
         console.log(id);
         const job = await service.list(id);
         return res.json({job})
     }
 
-    async listAll(req: Request, res: Response) {
+    async listAll(req: Request, res: Response): Promise<Response> {
         const jobList = await service.listAll();
         return res.json({jobList});
     }
 
-    async create(req: Request, res: Response) {
+    async create(req: Request, res: Response): Promise<Response> {
         const { prompt } = req.body;
         const job = await service.create(prompt);
         return res.status(202).json(job);
     }
 
-    async changeStatus(req: Request, res: Response) {
+    async changeStatus(req: Request, res: Response): Promise<Response> {
         const { id, status } = req.body;
         await service.changeStatus(id, status);
         return res.sendStatus(200);
